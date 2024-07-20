d2f34f3b85cfe44b7811bb63233548929235acb1
luisesales <duduosales@hotmail.com>
2024-07-20 12:34:27 -0300

    update to part 3 form 1

---
+++ src/test/groovy/online/contacts/book/DomainSpec.groovy
@@ -0,0 +1,18 @@
+package online.contacts.book
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class DomainSpec extends Specification implements DomainUnitTest<Domain> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
